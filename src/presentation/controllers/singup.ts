102bc2b9c7a9e7d72fcb0f8fd7c7c2a74ea515d5
Íttalo Lima Steltter <ittalolz@gmail.com>
2021-01-11 18:06:05 -0400

    feat: ensure SingUpController return 400 if no password confirmationconfirmation is provided

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -4,7 +4,7 @@ import { badRequest } from '../helpers/http-helper'
 
 export class SingUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requeiredFields = ['name', 'email', 'password']
+    const requeiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requeiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissimParamError(field))
