5e81d21d667d72969fc5460fbb3ebd193c384b2f
Íttalo Lima Steltter <ittalolz@gmail.com>
2021-01-11 18:00:27 -0400

    feat: ensure SingUpController return 400 if no password is provided

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -4,11 +4,11 @@ import { badRequest } from '../helpers/http-helper'
 
 export class SingUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissimParamError('name'))
-    }
-    if (!httpRequest.body.email) {
-      return badRequest(new MissimParamError('email'))
+    const requeiredFields = ['name', 'email', 'password']
+    for (const field of requeiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissimParamError(field))
+      }
     }
   }
 }
