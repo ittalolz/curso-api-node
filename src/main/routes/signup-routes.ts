1101edf64098618758936a1f0fc7c5f5e9389e78
Ittalo Lima Steltter <ittalolz@gmail.com>
2021-01-27 16:07:41 -0400

    feat: add ExpressRoutesAdapter

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express'
+import { makeSingUpController } from '../factories/signup'
+import { adaptetRoute } from '../adapters/express-routes-adapter'
 
 export default (router: Router): void => {
-  router.post('/signup', (req, res) => {
-    res.json({ ok: 'ok' })
-  })
+  // router.post('/signup', (req, res) => {
+  //   res.json({ ok: 'ok' })
+  // })
+  router.post('/signup', adaptetRoute(makeSingUpController()))
 }
