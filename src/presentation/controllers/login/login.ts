84d425cc201df8010bbdc80e7f235b37bb4e109c
Ittalo Lima Steltter <ittalolz@gmail.com>
2021-03-04 14:21:08 -0400

    refactor: eliminando codigo duplicado

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -11,12 +11,13 @@ export class LoginController implements Controller {
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    if (!httpRequest.body.email) {
+    const { email, password } = httpRequest.body
+    if (!email) {
       return new Promise(resolve => resolve(badRequest(new MissimParamError('email'))))
     }
-    if (!httpRequest.body.password) {
+    if (!password) {
       return new Promise(resolve => resolve(badRequest(new MissimParamError('password'))))
     }
-    this.emailValidator.isValid(httpRequest.body.email)
+    this.emailValidator.isValid(email)
   }
 }
