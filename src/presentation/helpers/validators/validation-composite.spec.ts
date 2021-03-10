797048c0226755ada0290987368415c38982fdbd
Ittalo Lima Steltter <ittalolz@gmail.com>
2021-03-10 15:25:22 -0400

    refactor: mover makesut para uma factory method

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -2,15 +2,33 @@ import { Validation } from '../../controllers/signup/signup-protocols'
 import { MissimParamError } from '../../errors'
 import { ValidationComposite } from './validation-composite'
 
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error {
+      return null
+    }
+  }
+  return new ValidationStub()
+}
+
+interface SutTypes {
+  sut: ValidationComposite
+  validationStub: Validation
+}
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidation()
+  const sut = new ValidationComposite([validationStub])
+  return {
+    sut,
+    validationStub
+  }
+}
+
 describe('Validation Composite', () => {
   test('Deve retornar um erro se qualquer validation falhar', () => {
-    class ValidationStub implements Validation {
-      validate (input: any): Error {
-        return new MissimParamError('field')
-      }
-    }
-    const validationStub = new ValidationStub()
-    const sut = new ValidationComposite([validationStub])
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissimParamError('field'))
     const error = sut.validate({ field: 'any_value' })
     expect(error).toEqual(new MissimParamError('field'))
   })
